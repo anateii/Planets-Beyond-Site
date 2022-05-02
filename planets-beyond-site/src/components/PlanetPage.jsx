8b2355539360df97216705348030c1d03d827ac4
Ana Matei <anamatei.099@gmail.com>
2022-05-02 23:53:36 +0100

    Toggle Sidebar, need to fix it

--- planets-beyond-site/src/components/PlanetPage.jsx
+++ planets-beyond-site/src/components/PlanetPage.jsx
@@ -2,12 +2,20 @@ import { Main } from "../styled-components/homepage"
 import starsBackground from "../assets/stars.svg"
 import cursor from "../assets/cursor.png"
 import Sidebar from "./Sidebar"
-
+import { useState } from "react"
 
 export const PlanetPage =() => {
+
+    const [sidebarOpen, setSideBarOpen] = useState(false);
+    const handleViewSidebar = () => {
+      setSideBarOpen(!sidebarOpen);
+    };
+
+
+
 return (
     <Main style={{backgroundImage: `url(${starsBackground})`, cursor: `url(${cursor}), auto`}}>
-       <Sidebar/>
+       <Sidebar  isOpen={sidebarOpen} toggleSidebar={handleViewSidebar}/>
            
     </Main>
 )
--- planets-beyond-site/src/components/Sidebar.jsx
+++ planets-beyond-site/src/components/Sidebar.jsx
@@ -1,28 +1,34 @@
 import { SideBar, Icon } from "../styled-components/planetpage";
 import rocket from "../assets/rocket.png";
 import { useNavigate } from "react-router-dom";
-import eye from "../assets/eye.png";
 
-const Sidebar = () => {
+const Sidebar = (props) => {
   const navigate = useNavigate();
 
+  const sidebarClass = props.isOpen ? "sidebar open" : "sidebar";
+
   return (
-    <SideBar>
-      <Icon>Overview</Icon>
-      <Icon>Internal Structure</Icon>
-      <Icon>Surface Geology</Icon>
-      <Icon>Video</Icon>
-      <Icon
-        onClick={() => {
-          navigate("/destination");
-        }}
-      >
-        <div>
-          <img src={rocket} alt="" />
-          Go back
-        </div>
-      </Icon>
-    </SideBar>
+    <>
+      <SideBar className={sidebarClass}>
+        <Icon>Overview</Icon>
+        <Icon>Internal Structure</Icon>
+        <Icon>Surface Geology</Icon>
+        <Icon>Video</Icon>
+        <Icon
+          onClick={() => {
+            navigate("/destination");
+          }}
+        >
+          <div>
+            <img src={rocket} alt="" />
+            Go back
+          </div>
+        </Icon>
+        <button onClick={props.toggleSidebar} className="sidebar-toggle">
+          Toggle Sidebar
+        </button>
+      </SideBar>
+    </>
   );
 };
 
