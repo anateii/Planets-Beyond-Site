74c38e52e3325754bd234c48e9fb3a7887d19c05
Ana Matei <anamatei.099@gmail.com>
2022-05-02 20:44:31 +0100

    Added PlanetPage styled-components

--- planets-beyond-site/src/components/PlanetPage.jsx
+++ planets-beyond-site/src/components/PlanetPage.jsx
@@ -1,11 +1,12 @@
 import { Main } from "../styled-components/homepage"
 import starsBackground from "../assets/stars.svg"
 import cursor from "../assets/cursor.png"
+import { Container} from "../styled-components/planetpage"
 
 export const PlanetPage =() => {
 return (
     <Main style={{backgroundImage: `url(${starsBackground})`, cursor: `url(${cursor}), auto`}}>
-        Hello
+        <Container></Container>
     </Main>
 )
 }
---
+++ planets-beyond-site/src/styled-components/planetpage.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+
+export const Container = styled.div `
+background-color: red; 
+height: 500px;
+width: 800px;
+
+`
