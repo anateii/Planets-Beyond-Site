1274ed647f869872c75b092c1f1a89dfc67c923b
Ana Matei <anamatei.099@gmail.com>
2022-05-11 11:57:54 +0100

    Updates

--- planets-beyond-site/src/components/Homepage.jsx
+++ planets-beyond-site/src/components/Homepage.jsx
@@ -1,8 +1,7 @@
 import { Main, Title, Button, Subtitle } from "../styled-components/homepage";
-import background from "../assets/background3.jpg"
+import background from "../assets/background3.jpg";
 import { useNavigate } from "react-router-dom";
 import cursor from "../assets/cursor.png";
-
 
 export const Homepage = () => {
   const navigate = useNavigate();
@@ -11,23 +10,24 @@ export const Homepage = () => {
     <Main
       style={{
         backgroundImage: `url(${background})`,
-       objectFit:"cover",
+        objectFit: "cover",
         cursor: `url(${cursor}), auto`,
       }}
-      animate={{opacity:[0.3,1]}}
+      animate={{ opacity: [0.3, 1]}}
+      transition={{duration: 1}}
     >
-      <Title animate={{y: [-300, 0]}}>
-       Planets Beyond
-        </Title>
-        <Subtitle  animate={{y: [-350, 0]}} >  
-            Curious about our Neighboors?
-            </Subtitle>
-   
+      <Title animate={{ y: [-300, 0] }} transition={{  duration: 1.5 }}>
+        Planets Beyond
+      </Title>
+      <Subtitle animate={{ y: [-350, 0] }} transition={{  duration: 2.5 }}>
+        Curious about our Neighboors?
+      </Subtitle>
 
       <Button
         onClick={() => navigate("/destination")}
         style={{ cursor: `url(${cursor}), auto` }}
-        animate={{opacity:[0,1]}}
+        animate={{ opacity: [0, 1] }}
+        transition={{ duration: 2 }}
       >
         Start your Journey
       </Button>
