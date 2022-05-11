060b4a4237a702010b968a37759355a94c826ef4
Ana Matei <anamatei.099@gmail.com>
2022-05-11 22:42:11 +0100

    Updated Homepage with variants

--- planets-beyond-site/src/components/Homepage.jsx
+++ planets-beyond-site/src/components/Homepage.jsx
@@ -3,23 +3,36 @@ import background from "../assets/background3.jpg";
 import { useNavigate } from "react-router-dom";
 import cursor from "../assets/cursor.png";
 
-const buttonVariants = {
-initial: {
-  y: '-300vw', 
-  opacity: 0
-},
-animate: {
- y: 0, 
-  opacity:1
-}
-}
+const mainVariants = {
+  initial: {
+    opacity: 0.6,
+  },
+  animated: {
+    opacity: 1,
+    transition: {
+      duration: 0.7,
+      type: "spring",
+    },
+  },
+};
 
+const buttonVariants = {
+  initial: {
+    y: "-300vw",
+    opacity: 0,
+  },
+  animated: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 2,
+      type: "spring",
+    },
+  },
+};
 
 export const Homepage = () => {
   const navigate = useNavigate();
-
-
-
 
   return (
     <Main
@@ -28,34 +41,28 @@ export const Homepage = () => {
         objectFit: "cover",
         cursor: `url(${cursor}), auto`,
       }}
-      animate={{ opacity: [0.6, 1]}}
-      transition={{duration: 0.7, type: 'spring'}}
+      variants={mainVariants}
+      initial="initial"
+      animate="animated"
     >
-      <Title animate={{ y: [-300, 0] }} transition={{  duration: 2.5 }}>
+      <Title animate={{ y: [-300, 0] }} transition={{ duration: 2.5 }}>
         Planets Beyond
       </Title>
-      <Subtitle animate={{ y: [-350, 0] }} transition={{  duration: 1.5 }}>
+      <Subtitle animate={{ y: [-350, 0] }} transition={{ duration: 1.5 }}>
         Curious about our Neighboors?
       </Subtitle>
 
       <Button
         onClick={() => navigate("/destination")}
         style={{ cursor: `url(${cursor}), auto` }}
-
-       /*  initial={{y: '-300vw'}}
-        animate={{ opacity: [0, 1] , y: 0}} */
         variants={buttonVariants}
         initial="initial"
-        animate="animate"
-        transition={{duration: 2 , type: 'spring'}}
+        animate="animated"
         whileHover={{
           scale: 1.1,
-          textShadow: '0px 0px 15px rgb(7,7,36,0.5)',
-          boxShadow: '0px 0px 15px 1px rgb(7,7,36,0.3)',
-        
+          textShadow: "0px 0px 15px rgb(7,7,36,0.5)",
+          boxShadow: "0px 0px 15px 1px rgb(7,7,36,0.3)",
         }}
-        
-        
       >
         Start your Journey
       </Button>
