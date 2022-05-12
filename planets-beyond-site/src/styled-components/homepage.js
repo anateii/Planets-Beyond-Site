6494d0f5bee1f5e4a74e4beb2939c3de077a6939
Ana Matei <anamatei.099@gmail.com>
2022-05-12 16:50:59 +0100

    Started Responsivity

--- planets-beyond-site/src/styled-components/homepage.js
+++ planets-beyond-site/src/styled-components/homepage.js
@@ -17,6 +17,12 @@ export const Title = styled(motion.h1)`
   top: 30%;
  font-family: 'League Spartan', sans-serif;
   font-size: 4em;
+  @media screen and (max-width: 425px) {
+    font-size: 3em;
+    display: flex;
+    flex-wrap: wrap;
+    line-height: initial;
+  }
 `;
 
 export const Subtitle = styled(motion.h3) `
@@ -27,13 +33,26 @@ font-family: 'Antonio', sans-serif;
 font-size: 2rem;
 font-weight: 400;
 margin: 0 auto;
+@media screen and (max-width: 425px) {
+  left: 10%;
+  top: 55%;
+  font-size: 1em;
+};
+@media screen and (min-width: 425px, max-width: 768px) {
+  left: 28%
+};
+
 
 `;
 
 
 
 export const Button = styled(motion.button)`
-  background-color: #fff0;
+@media screen and (max-width: 425px) {
+  top: 60%;
+};  
+
+background-color: #fff0;
   color: #fff;
   top: 45%;
   position: relative;
